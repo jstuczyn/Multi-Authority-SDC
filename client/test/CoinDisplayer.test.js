18fcc9ba08c4392667494744f11fceb102d7d1b3
jstuczyn <jedrzej.stuczynski@gmail.com>
2017-12-23 23:57:27 +0100

    Changed structure of tests for CoinDisplayer Component to not recreate coin every time

--- client/test/CoinDisplayer.test.js
+++ client/test/CoinDisplayer.test.js
@@ -4,25 +4,28 @@ import {shallow, mount, render} from 'enzyme';
 import CoinDisplayer from '../src/components/CoinDisplayer';
 import MainView from '../src/components/MainView';
 
+let coinDisplayerNode;
+
 describe('CoinDisplayer Component', () => {
-    describe('Should have received Coin as a prop', () => {
-        const coinValue = 42;
+    const coinValue = 42;
+    before(() => {
         const wrapper = mount(<MainView/>);
         wrapper.find('input').simulate('change', {target: {value: coinValue}});
         wrapper.find('button').simulate('click');
 
-        const coinDisplayerNode = wrapper.find(CoinDisplayer);
-
+        coinDisplayerNode = wrapper.find(CoinDisplayer);
+    });
+    describe('Should have received Coin as a prop', () => {
         it('That has TTL in a future', () => {
-            expect(coinDisplayerNode.props.coin.ttl).toBeGreaterThan(new Date().getTime());
+            expect(coinDisplayerNode.props().coin.ttl > new Date().getTime()).to.equal(true);
         });
 
         it('That has the same value as from the input', () => {
-            expect(coinDisplayerNode.props.coin.value).to.equal(coinValue);
+            expect(coinDisplayerNode.props().coin.value).to.equal(coinValue);
         });
 
     });
-
+    
     // more to come as Component is developed
 
 });
