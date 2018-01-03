59ceba703780a51f014d97ec9af262c89c494c61
jstuczyn <jedrzej.stuczynski@gmail.com>
2018-01-03 13:41:03 +0100

    Changed CoinListDisplayer to be a stateless component and pass sk to CoinDisplayer

--- client/src/components/CoinListDisplayer.js
+++ client/src/components/CoinListDisplayer.js
@@ -2,22 +2,13 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import CoinDisplayer from './CoinDisplayer';
 
-class CoinListDisplayer extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    // {sk: sk, coin: coin}
-    return (
-      <div>
-        {this.props.coins.map(coin => (
-          <CoinDisplayer key={coin.coin.id} coin={coin.coin} />
-        ))}
-      </div>
-    );
-  }
-}
+const CoinListDisplayer = props => (
+  <div>
+    {props.coins.map(coin => (
+      <CoinDisplayer key={coin.coin.id} coin={coin.coin} sk={coin.sk} />
+    ))}
+  </div>
+);
 
 CoinListDisplayer.propTypes = {
   coins: PropTypes.array.isRequired,
