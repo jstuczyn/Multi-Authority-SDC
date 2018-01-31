5df58f2ebb02fc250779fbf1f3dc890fb836c039
jstuczyn <jedrzej.stuczynski@gmail.com>
2018-01-31 13:22:03 +0000

    Added issue status messages

---
+++ servers/src/issuer/config/constants.js
@@ -0,0 +1,9 @@
+export const ISSUE_ERROR_NOT_ENOUGH_BALANCE = 'Balance was not high enough to issue the coin';
+export const ISSUE_ERROR_PROOF_INVALID = 'Proof of secret was invalid';
+export const ISSUE_SUCCESS = 'Coin was successfully issued';
+
+export const ISSUE_STATUS = {
+  error_balance: ISSUE_ERROR_NOT_ENOUGH_BALANCE,
+  error_proof: ISSUE_ERROR_PROOF_INVALID,
+  success: ISSUE_SUCCESS,
+};
