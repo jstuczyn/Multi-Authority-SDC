25db4045f4a1d01460d73ff527c05e66e78dce9a
jstuczyn <jedrzej.stuczynski@gmail.com>
2018-01-31 13:20:48 +0000

    Renamed key variables for lesser confusion

--- servers/src/issuer/config/KeySetup.js
+++ servers/src/issuer/config/KeySetup.js
@@ -1,18 +1,18 @@
 import { ctx, params } from '../../config';
 import { DEBUG } from './appConfig';
 
-export const keys = {};
-export const pkBytes = [];
-export const skBytes = [];
+export const sigKeys = {};
+export const sig_pkBytes = [];
+export const sig_skBytes = [];
 
 export const setupKeys = () => {
   const [G, o, g1, g2, e] = params;
 
   // todo: when refactoring entire codebase, change all randomnum calls to that
-  keys.sk = ctx.BIG.randomnum(o, G.rngGen);
-  keys.sk.toBytes(skBytes);
-  keys.pk = g1.mul(keys.sk);
-  keys.pk.toBytes(pkBytes);
+  sigKeys.sk = ctx.BIG.randomnum(o, G.rngGen);
+  sigKeys.sk.toBytes(sig_skBytes);
+  sigKeys.pk = g1.mul(sigKeys.sk);
+  sigKeys.pk.toBytes(sig_pkBytes);
 
   if (DEBUG) {
     console.log('Generated sk,pk keypair.');
