0618c64c53fdeaa4b583956351ef4dc5bd823282
jstuczyn <jedrzej.stuczynski@gmail.com>
2018-02-03 20:19:16 +0000

    Changed initial migration to include pkey constraint

--- servers/issuerdb/migrations/20180128161132_initial.js
+++ servers/issuerdb/migrations/20180128161132_initial.js
@@ -1,13 +1,14 @@
 
 exports.up = function(knex, Promise) {
   return knex.schema.createTable('tblGeneratedIds', (table) => {
-    table.string('Id').notNull();
+    table.string('Id').notNull().primary();
   }).createTable('tblUsedIds', (table) => {
-    table.string('Id').notNull();
+    table.string('Id').notNull().primary();
   }).createTable('tblClients', (table) => {
     table.string('Address').notNull();
     table.string('Name').notNull();
     table.decimal('Balance', 16, 2).notNull();
+    table.primary(['Name', 'Address']);
   });
 };
 
