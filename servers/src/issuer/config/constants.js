f010c53b57886fb8f9e9977dd6fb97e21041a92b
jstuczyn <jedrzej.stuczynski@gmail.com>
2018-02-12 23:55:39 +0000

    Extra error code for invalid signature

--- servers/src/issuer/config/constants.js
+++ servers/src/issuer/config/constants.js
@@ -1,9 +1,11 @@
-export const ISSUE_ERROR_NOT_ENOUGH_BALANCE = 'Balance was not high enough to issue the coin';
-export const ISSUE_ERROR_PROOF_INVALID = 'Proof of secret was invalid';
-export const ISSUE_SUCCESS = 'Coin was successfully issued';
+const ISSUE_ERROR_NOT_ENOUGH_BALANCE = 'Balance was not high enough to issue the coin';
+const ISSUE_ERROR_PROOF_INVALID = 'Proof of secret was invalid';
+const ISSUE_SUCCESS = 'Coin was successfully issued';
+const ISSUE_ERROR_INVALID_SIGNATURE = 'The signature on request was invalid';
 
 export const ISSUE_STATUS = {
   error_balance: ISSUE_ERROR_NOT_ENOUGH_BALANCE,
   error_proof: ISSUE_ERROR_PROOF_INVALID,
   success: ISSUE_SUCCESS,
+  error_signature: ISSUE_ERROR_INVALID_SIGNATURE,
 };
