32f77d0f684cabd8f8bd4e20a91fa89217b939da
jstuczyn <jedrzej.stuczynski@gmail.com>
2018-02-12 22:56:34 +0000

    Generating clients sk,pk pair

--- client/src/components/MainView.js
+++ client/src/components/MainView.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { Grid, Segment, Header } from 'semantic-ui-react';
 import CoinRequester from './CoinRequester';
 import CoinListDisplayer from './CoinListDisplayer';
-// import { getCoin } from '../utils/coinGenerator';
 import { getCoin } from '../utils/api';
 import Coin from '../../lib/Coin';
 import { params, DEBUG, DETAILED_DEBUG, issuer } from '../config';
@@ -20,16 +19,34 @@ class MainView extends React.Component {
 
   componentWillMount() {
     // generate ElGamal keypair
-    const [sk, pk] = ElGamal.keygen(params);
+    const [sk_elgamal, pk_elgamal] = ElGamal.keygen(params);
+    const [G, o, g1, g2, e] = params;
+
+    // due to way ECDSA is implemented, theres no point in storing the other representation,
+    // bytes are enough
+    const skBytes_client = [];
+    const pkBytes_client = [];
+    // generate keypair for signing messages
+    const sk_client = G.ctx.BIG.randomnum(o, G.rngGen);
+    sk_client.toBytes(skBytes_client);
+    const pk_client = g1.mul(sk_client);
+    pk_client.toBytes(pkBytes_client);
+
     this.setState({
-      ElGamalSK: sk,
-      ElGamalPK: pk,
+      ElGamalSK: sk_elgamal,
+      ElGamalPK: pk_elgamal,
+      sk_client: skBytes_client,
+      pk_client: pkBytes_client,
     });
 
     if (DEBUG) {
       console.log('Generated ElGamal keypair.');
       if (DETAILED_DEBUG) {
-        console.log('Keys:', sk, pk);
+        console.log('Keys:', sk_elgamal, pk_elgamal);
+      }
+      console.log('Generated client keypair.');
+      if (DETAILED_DEBUG) {
+        console.log('Keys:', skBytes_client, pkBytes_client);
       }
     }
   }
