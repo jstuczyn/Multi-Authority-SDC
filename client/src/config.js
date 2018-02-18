6b98f5783c75ffb0228a113ddad5f84b71fe2eb4
jstuczyn <jedrzej.stuczynski@gmail.com>
2018-02-18 21:05:46 +0000

    Fixed swaped ips

--- client/src/config.js
+++ client/src/config.js
@@ -14,9 +14,10 @@ export const signingServers = (process.env.NODE_ENV === 'production') ? [
   '127.0.0.1:3002',
 ];
 
-export const merchant = (process.env.NODE_ENV === 'production') ? '35.178.63.67:3001' : '127.0.0.1:4000';
+export const merchant = (process.env.NODE_ENV === 'production') ? '35.177.203.169:3001' : '127.0.0.1:4000';
 
-export const issuer = (process.env.NODE_ENV === 'production') ? '35.177.203.169:3002' : '127.0.0.1:5000';
+export const issuer = (process.env.NODE_ENV === 'production') ? '35.178.63.67:3002' : '127.0.0.1:5000';
+
 
 
 // even though it is set a const, it can still be mutated because it is an object;
--- servers/src/globalConfig.js
+++ servers/src/globalConfig.js
@@ -11,9 +11,10 @@ export const signingServers = (process.env.NODE_ENV === 'production') ? [
   '127.0.0.1:3002',
 ];
 
-export const merchant = (process.env.NODE_ENV === 'production') ? '35.178.63.67:3001' : '127.0.0.1:4000';
+export const merchant = (process.env.NODE_ENV === 'production') ? '35.177.203.169:3001' : '127.0.0.1:5000';
 
-export const issuer = (process.env.NODE_ENV === 'production') ? '35.177.203.169:3002' : '127.0.0.1:5000';
+export const issuer = (process.env.NODE_ENV === 'production') ? '35.178.63.67:3002' : '127.0.0.1:4000';
+
 
 export const ctx = new CTX('BN254');
 export const params = CoinSig.setup();
