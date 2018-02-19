541f0b8fdc4c10cbf1e7a8b50a3d7b7fa0278000
Jędrzej Stuczyński <jedrzej.stuczynski@gmail.com>
2018-02-19 09:29:35 +0000

    Updated IPs after assigning elastic IPs to all AWS instances

--- client/src/config.js
+++ client/src/config.js
@@ -5,18 +5,18 @@ export const DEBUG = true;
 export const DETAILED_DEBUG = true;
 
 export const signingServers = (process.env.NODE_ENV === 'production') ? [
-  '35.177.85.84:3000',
-  '35.177.164.153:3000',
-  '35.176.145.134:3000',
+  '35.176.12.245:3000',
+  '35.177.49.8:3000',
+  '35.177.54.142:3000',
 ] : [
   '127.0.0.1:3000',
   '127.0.0.1:3001',
   '127.0.0.1:3002',
 ];
 
-export const merchant = (process.env.NODE_ENV === 'production') ? '35.177.203.169:3001' : '127.0.0.1:4000';
+export const merchant = (process.env.NODE_ENV === 'production') ? '35.178.0.223:3001' : '127.0.0.1:4000';
 
-export const issuer = (process.env.NODE_ENV === 'production') ? '35.178.63.67:3002' : '127.0.0.1:5000';
+export const issuer = (process.env.NODE_ENV === 'production') ? '35.178.15.103:3002' : '127.0.0.1:5000';
 
 
 
