53ec0ce42c1b1f8aba8b989311b3d7686e1b504e
jstuczyn <jedrzej.stuczynski@gmail.com>
2018-02-22 18:34:32 +0000

    Fixed tests for App component

--- client/test/App.test.js
+++ client/test/App.test.js
@@ -4,20 +4,29 @@ import { expect } from 'chai';
 import { shallow, mount, render } from 'enzyme';
 import App from '../src/components/App';
 import MainView from '../src/components/MainView';
+import TopMenu from '../src/components/TopMenu';
+import ServerStatuses from '../src/components/ServerStatuses';
+import Footer from '../src/components/Footer';
 
 describe('App Component', () => {
-  it('Should have two children', () => {
+  it('Should have three children', () => {
     const wrapper = shallow(<App />);
-    expect(wrapper.children().length).to.equal(2);
+    expect(wrapper.children().length).to.equal(3);
   });
 
-  it('Contains a Header component', () => {
+  it('Contains a TopMenu component', () => {
     const wrapper = mount(<App />);
-    expect(wrapper.find(Header)).to.have.length(1);
+    expect(wrapper.find(TopMenu)).to.have.length(1);
   });
 
-  it('Contains a MainView component', () => {
+  it('Contains a Footer component', () => {
     const wrapper = mount(<App />);
-    expect(wrapper.find(MainView)).to.have.length(1);
+    expect(wrapper.find(Footer)).to.have.length(1);
+  });
+
+  it('Contains a div with MainView and ServerStatuses components', () => {
+    const wrapper = mount(<App />);
+    expect(wrapper.find('div').find(MainView)).to.have.length(1);
+    expect(wrapper.find('div').find(ServerStatuses)).to.have.length(1);
   });
 });
